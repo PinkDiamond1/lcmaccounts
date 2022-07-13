e33f102895f933df79575fdf16b94ac185135193
Michael Harrison <volo@rice.edu>
2022-07-13 13:31:54 -0500

    routes and locales update

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 # rubocop:disable Metrics/BlockLength
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'static_pages#home'
+  root to: 'sessions#welcome'
+
+  mount OpenStax::Api::Engine, at: '/'
 
   # routes to old faculty access controller, redirect them to the sheerid form or pending cs paths
   get 'faculty_access/apply/' => redirect('signup/educator/apply')
   get 'faculty_access/pending/' => redirect('signup/educator/pending_cs_verification')
+  get 'exit_accounts' => redirect('logout')
+  scope 'signup' do
+    get '/' => redirect('signup')
+  end
 
   direct :salesforce_knowledge_base do
     'https://openstax.secure.force.com/help/articles/FAQ/Can-t-log-in-to-your-OpenStax-account'
   end
-
+  
   scope controller: :profile do
     # Profile access
     get 'profile'
@@ -30,51 +36,27 @@ Rails.application.routes.draw do
   scope controller: 'signup' do
     get 'signup', action: :welcome
     get 'done', action: :signup_done, as: :signup_done
+
+    # verification actions that are not part of signup per se
     get 'verify_email_by_code/:code', action: :verify_email_by_code, as: :verify_email_by_code
     get 'check_your_email'
   end
 
-  scope controller: :student_signup do
-    get 'signup/student', action: :student_signup_form, as: :signup_student
-    post 'signup/student', action: :student_signup, as: :signup_post
-
-    get 'signup/student/email_verification_form', action: :student_email_verification_form, as: :student_email_verification_form
-    post 'signup/student/change_signup_email', action: :student_change_signup_email, as: :student_change_signup_email
-    get 'signup/student/email_verification_form_updated_email',
-      action: :student_email_verification_form_updated_email,
-      as: :student_email_verification_form_updated_email
-    get 'signup/student/change_signup_email_form', action: :student_change_signup_email_form, as: :student_change_signup_email_form
-    post 'signup/student/verify_email_by_pin', action: :student_verify_email_by_pin, as: :student_verify_pin
-  end
-
   scope controller: :educator_signup do
-    # Step 1
-    get 'signup/educator', action: :educator_signup_form, as: :educator_signup
-    post 'signup/educator', action: :educator_signup, as: :educator_signup_post
-    get 'signup/educator/change_signup_email_form', action: :educator_change_signup_email_form, as: :educator_change_signup_email_form
-    post 'signup/educator/change_signup_email', action: :educator_change_signup_email, as: :educator_change_signup_email
-
-    # Step 2
-    get 'signup/educator/email_verification_form', action: :educator_email_verification_form, as: :educator_email_verification_form
-    get 'signup/educator/email_verification_form_updated_email',
-      action: :educator_email_verification_form_updated_email,
-      as: :educator_email_verification_form_updated_email
-    post 'signup/educator/verify_email_by_pin', action: :educator_verify_email_by_pin, as: :educator_verify_pin
-
     # Step 3
-    get 'signup/educator/apply', action: :educator_sheerid_form, as: :educator_sheerid_form
+    get 'signup/educator/apply', action: :sheerid_form, as: :sheerid_form
     post 'sheerid/webhook', action: :sheerid_webhook, as: :sheerid_webhook
 
     # Step 4
-    get 'signup/educator/profile_form', action: :educator_profile_form, as: :educator_profile_form
-    post 'signup/educator/complete_profile', action: :educator_complete_profile, as: :educator_complete_profile
-    get 'signup/educator/pending_cs_verification', action: :educator_pending_cs_verification, as: :educator_pending_cs_verification
+    get 'signup/educator/profile_form', action: :profile_form, as: :profile_form
+    post 'signup/educator/complete_profile', action: :profile_form_post, as: :complete_profile_post
+    get 'signup/educator/pending_cs_verification', action: :pending_cs_verification, as: :cs_verification
 
-    get 'signup/educator/cs_form', action: :educator_profile_form, as: :educator_cs_verification_form
-    post 'signup/educator/cs_verification_request', action: :educator_complete_profile, as: :educator_cs_verification_request
+    get 'signup/educator/cs_form', action: :pending_cs_verification_form, as: :cs_verification_form
+    post 'signup/educator/cs_verification_request', action: :profile_form_post, as: :cs_verification_post
   end
 
-  scope controller: :password_management do
+  scope controller: :passwords do
     # Password management process (forgot,  change, or create password)
     get 'forgot_password_form'
     post 'send_reset_password_email'
@@ -132,6 +114,25 @@ Rails.application.routes.draw do
     get 'continue'
   end
 
+  scope controller: :social_auth do
+    get 'auth/:provider', action: :oauth_callback, as: :social_auth
+    post 'auth/:provider', action: :oauth_callback
+    get 'auth/:provider/callback', action: :oauth_callback
+    delete 'auth/:provider', action: :remove_auth_strategy
+    #   When you sign up with a social provider, you must confirm your info first
+    get 'confirm_oauth_info', action: :confirm_oauth_info
+    post 'confirm_oauth_info', action: :confirm_oauth_info
+  end
+
+  # Create a named path for links like `/auth/facebook` so that the path prefixer gem
+  #     will appropriately prefix the path. https://stackoverflow.com/a/40125738/1664216
+  # The actual request, however, is handled by the omniauth middleware when it detects
+  #     that the current_url is the callback_path, using `OmniAuth::Strategy#on_callback_path?`
+  #     So, admittedly, this route is deceiving.
+  get '/auth/:provider', to: ->(_env) { [404, {}, ['Not Found']] }, as: :oauth
+
+
+
   resources :contact_infos, only: [:create, :destroy] do
     member do
       put 'set_searchable'
@@ -174,37 +175,36 @@ Rails.application.routes.draw do
       end
     end
 
-    # resources :application_groups, only: [] do
-    #   collection do
-    #     get 'updates'
-    #     put 'updated'
-    #   end
-    # end
-    #
-    # resources :groups, only: [:index, :show, :create, :update, :destroy] do
-    #   post '/members/:user_id', to: 'group_members#create'
-    #   delete '/members/:user_id', to: 'group_members#destroy'
-    #
-    #   post '/owners/:user_id', to: 'group_owners#create'
-    #   delete '/owners/:user_id', to: 'group_owners#destroy'
-    #
-    #   post '/nestings/:member_group_id', to: 'group_nestings#create'
-    #   delete '/nestings/:member_group_id', to: 'group_nestings#destroy'
-    # end
-    #
-    # resources :group_members, only: [:index], path: 'memberships'
-    # resources :group_owners, only: [:index], path: 'ownerships'
+    resources :application_groups, only: [] do
+      collection do
+        get 'updates'
+        put 'updated'
+      end
+    end
 
-    get 'raise_exception/:type', to: 'dev#raise_exception' unless Rails.env.production?
+    resources :groups, only: [:index, :show, :create, :update, :destroy] do
+      post '/members/:user_id', to: 'group_members#create'
+      delete '/members/:user_id', to: 'group_members#destroy'
+
+      post '/owners/:user_id', to: 'group_owners#create'
+      delete '/owners/:user_id', to: 'group_owners#destroy'
+
+      post '/nestings/:member_group_id', to: 'group_nestings#create'
+      delete '/nestings/:member_group_id', to: 'group_nestings#destroy'
+    end
+
+    resources :group_members, only: [:index], path: 'memberships'
+    resources :group_owners, only: [:index], path: 'ownerships'
   end
 
   use_doorkeeper { controllers applications: 'oauth/applications' }
 
-  mount FinePrint::Engine => '/admin/fine_print'
+  mount FinePrint::Engine => 'admin/fine_print'
+  mount Blazer::Engine, at: 'admin/blazer', as: 'blazer_admin'
+  match "admin/job_dashboard" => DelayedJobWeb, :anchor => false, :via => [:get, :post]
   mount OpenStax::Utilities::Engine => :status
 
   namespace 'admin' do
-    get '/', to: 'base#index'
     get '/console', to: 'console#index'
 
     put 'cron', to: 'base#cron'
@@ -216,24 +216,12 @@ Rails.application.routes.draw do
       get 'js_search', on: :collection
       get 'actions', on: :collection
       put 'mark_users_updated', on: :collection
-      post 'force_update_lead', on: :member
     end
-
-    resource :reports, only: [:show]
 
     resource :security_log, only: [:show]
 
-    delete :delete_contact_info, path: '/contact_infos/:id/verify',
-                                 controller: :contact_infos, action: :destroy
-    post :verify_contact_info, path: '/contact_infos/:id/verify',
-                               controller: :contact_infos, action: :verify
-
-    resources :pre_auth_states, only: [:index]
-
-    resources :banners, except: :show
-
-    mount Blazer::Engine, at: 'blazer', as: 'blazer_admin'
-    match "/job_dashboard" => DelayedJobWeb, :anchor => false, :via => [:get, :post]
+    delete :delete_contact_info, path: '/contact_infos/:id/verify', controller: :contact_infos, action: :destroy
+    post :verify_contact_info, path: '/contact_infos/:id/verify', controller: :contact_infos, action: :verify
   end
 
   namespace 'dev' do
